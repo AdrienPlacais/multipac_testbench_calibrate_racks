776d2a02f163ea1659e2dc05b1a343da5facafd1
Adrien P <placais@lpsc.in2p3.fr>
2023-11-20 17:20:37 +0100

    with url of github repo

--- src/set_of_racks.py
+++ src/set_of_racks.py
@@ -54,6 +54,7 @@ class SetOfRacks(list):
         header = f"""# File created on {datetime.now()}.
 # Created with "multipac_testbench_calibrate_rf_racks" Python script, available at:
 # https://gitlab.in2p3.fr/multipactor/calibrate_rf_racks.git
+# https://github.com/AdrienPlacais/multipac_testbench_calibrate_racks
 #
 # For any question/remark: placais@lpsc.in2p3.fr
 #
